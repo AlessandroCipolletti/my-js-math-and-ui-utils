3c41c64bc95e53df0a9cfcfa6762e0263c89781a
Alessandro Cipolletti <cipolletti.alessandro@gmail.com>
2022-05-08 10:33:08 +0200

    Animation Utils jsDoc

--- src/animationsUtils.ts
+++ src/animationsUtils.ts
@@ -1,6 +1,6 @@
 import { iterateFn } from './jsUtils'
 
-const FADE_TRANSITION_DEFAULT_DURATION = 250 // ms
+const DEFAULT_FADE_TRANSITION_DURATION = 250 // ms
 
 /**
  * A small animation api to uniform things like fadeIn / fadeOut animations.
@@ -38,6 +38,7 @@ interface AnimationsOptions {
 }
 
 /**
+ * @const animationsKeyframes
  * Keyframes for some default animations: fadeIn and fadeOut
  */
 const animationsKeyframes: KeyframesAnimations = {
@@ -49,9 +50,16 @@ const animationsKeyframes: KeyframesAnimations = {
     { opacity: maxFadeIn, offset: 0 },
     { opacity: 0, offset: 1 },
   ]),
+  addIn: () => ([
+    { width: '0px', height: '0px', marginTop: '0px', offset: 0 },
+  ]),
+  removeOut: () => ([
+    { width: '0px', height: '0px', marginTop: '0px', offset: 1 },
+  ]),
 }
 
 /**
+ * @function animateElement
  * Animates the given element with the given keyframes.
  * It needs also an animationNane; with this you will be able to check which animation is running,
  * and stop them one by one.
@@ -109,8 +117,12 @@ export const animateElement = async(
 }
 
 /**
+ * @function elementHasAnimation
  * Checks if the given Dom element has a certain animation in progress.
  * This allows to prevent the same animation to be played multiple times at the same time.
+ *
+ * @example
+ * elementHasAnimation(myDom, 'fadeIn') // ==> true | false
  *
  * @param {HTMLElementWithAnimations} element
  * @param {string} animName
@@ -127,7 +139,11 @@ const elementHasAnimation = (
 }
 
 /**
+ * @function cancelElementAnimationIfExists
  * Cancel an animation, if it's still in progress
+ *
+ * @example
+ * cancelElementAnimationIfExists(myDom, 'fadeIn')
  *
  * @param {HTMLElementWithAnimations} element
  * @param {string} animName
@@ -146,6 +162,7 @@ export const cancelElementAnimationIfExists = (
 }
 
 /**
+ * @function _fadeInEl
  * Local util to perform a fadeId animation on an element
  *
  * @param {HTMLElementWithAnimations} element
@@ -171,6 +188,7 @@ const _fadeInEl = async(
 }
 
 /**
+ * @function _fadeOutEl
  * Local util to perform a fadeOut animation on an element
  *
  * @param {HTMLElementWithAnimations} element
@@ -198,6 +216,7 @@ const _fadeOutEl = async(
 }
 
 /**
+ * @function _toggleFadeEl
  * Local util to perform a toggle fade animation on an element
  *
  * @param {HTMLElementWithAnimations} element
@@ -222,7 +241,14 @@ const _toggleFadeEl = async(
 }
 
 /**
+ * @function fadeInElements
  * Performs a fadeIn animation on one or multiple Dom elements at the same time.
+ *
+ * @example
+ * fadeInElements(myDom, { duration: 400 })
+ * await fadeInElements([myDom1, myDom2])
+ * await fadeInElements([myDom1, myDom2], { maxFadeIn: 0.75 })
+ * fadeInElements(myDom, { duration: 400, commitResult: false })
  *
  * @param {HTMLElementWithAnimations|Array<HTMLElementWithAnimations>} elements
  * @params {AnimationsOptions} options
@@ -232,7 +258,7 @@ export const fadeInElements = async(
   elements: HTMLElementWithAnimations|Array<HTMLElementWithAnimations>,
   options: AnimationsOptions = {},
 ): Promise<void> => {
-  const duration = options.duration || FADE_TRANSITION_DEFAULT_DURATION
+  const duration = options.duration || DEFAULT_FADE_TRANSITION_DURATION
   const maxFadeIn = options.maxFadeIn || 1
   const commitResult = options.commitResult || true
 
@@ -240,7 +266,14 @@ export const fadeInElements = async(
 }
 
 /**
+ * @function fadeOutElements
  * Performs a fadeOut animation on one or multiple Dom elements at the same time.
+ *
+ * @example
+ * fadeOutElements(myDom, { duration: 400 })
+ * await fadeOutElements([myDom1, myDom2])
+ * await fadeOutElements([myDom1, myDom2], { maxFadeIn: 0.75 })
+ * fadeOutElements(myDom, { duration: 400, commitResult: false })
  *
  * @param {HTMLElementWithAnimations|Array<HTMLElementWithAnimations>} elements
  * @params {AnimationsOptions} options
@@ -250,7 +283,7 @@ export const fadeOutElements = async(
   elements: HTMLElementWithAnimations|Array<HTMLElementWithAnimations>,
   options: AnimationsOptions = {},
 ): Promise<void> => {
-  const duration = options.duration || FADE_TRANSITION_DEFAULT_DURATION
+  const duration = options.duration || DEFAULT_FADE_TRANSITION_DURATION
   const maxFadeIn = options.maxFadeIn || 1
   const commitResult = options.commitResult || true
 
@@ -258,7 +291,14 @@ export const fadeOutElements = async(
 }
 
 /**
+ * @function toggleFadeElements
  * Performs a toggle-fade animation on one or multiple Dom elements at the same time.
+ *
+ * @example
+ * toggleFadeElements(myDom, { duration: 400 })
+ * await toggleFadeElements([myDom1, myDom2])
+ * await toggleFadeElements([myDom1, myDom2], { maxFadeIn: 0.75 })
+ * toggleFadeElements(myDom, { duration: 400, commitResult: false })
  *
  * @param {HTMLElementWithAnimations|Array<HTMLElementWithAnimations>} elements
  * @params {AnimationsOptions} options
@@ -268,7 +308,7 @@ export const toggleFadeElements = async(
   elements: HTMLElementWithAnimations|Array<HTMLElementWithAnimations>,
   options: AnimationsOptions = {},
 ): Promise<void> => {
-  const duration = options.duration || FADE_TRANSITION_DEFAULT_DURATION
+  const duration = options.duration || DEFAULT_FADE_TRANSITION_DURATION
   const maxFadeIn = options.maxFadeIn || 1
   const commitResult = options.commitResult || true
 
@@ -276,6 +316,7 @@ export const toggleFadeElements = async(
 }
 
 /**
+ * @function _addInEl
 * Local util to perform a addIn animation on an element
 *
 * @param {HTMLElementWithAnimations} element
@@ -290,17 +331,20 @@ const _addInEl = async(
   maxFadeIn: number,
   commitResult: boolean,
 ): Promise<void> => {
-  element.classList.add('displayNone')
-  animateElement(element, 'addIn', [
-    { width: '0px', height: '0px', marginTop: '0px', offset: 0 },
-  ], duration, {
-    easing: 'ease-in-out',
-  }, commitResult)
-  await fadeInElements(element, { duration, maxFadeIn, commitResult })
-  cancelElementAnimationIfExists(element, 'addIn')
+  if (element instanceof HTMLElement) {
+    cancelElementAnimationIfExists(element, 'removeOut')
+    if (!elementHasAnimation(element, 'addIn')) {
+      element.classList.add('displayNone')
+      const keyframes = animationsKeyframes.addIn()
+      animateElement(element, 'addIn', keyframes, duration, { easing: 'ease-in-out' }, commitResult)
+      await fadeInElements(element, { duration, maxFadeIn, commitResult })
+      cancelElementAnimationIfExists(element, 'addIn')
+    }
+  }
 }
 
 /**
+ * @function _removeOutEl
 * Local util to perform a removeOut animation on an element
 *
 * @param {HTMLElementWithAnimations} element
@@ -315,18 +359,27 @@ export const _removeOutEl = async(
   maxFadeIn: number,
   commitResult: boolean,
 ): Promise<void> => {
-  animateElement(element, 'removeOut', [
-    { width: '0px', height: '0px', marginTop: '0px', offset: 1 },
-  ], duration, {
-    easing: 'ease-in-out',
-  }, commitResult)
-  await fadeOutElements(element, { duration, maxFadeIn, commitResult })
-  cancelElementAnimationIfExists(element, 'removeOut')
-  element.remove()
+  if (element instanceof HTMLElement) {
+    cancelElementAnimationIfExists(element, 'addIn')
+    if (!elementHasAnimation(element, 'removeOut')) {
+      const keyframes = animationsKeyframes.removeOut()
+      animateElement(element, 'removeOut', keyframes, duration, { easing: 'ease-in-out' }, commitResult)
+      await fadeOutElements(element, { duration, maxFadeIn, commitResult })
+      cancelElementAnimationIfExists(element, 'removeOut')
+      element.remove()
+    }
+  }
 }
 
 /**
+ * @function addInElements
  * Performs a addIn animation on one or multiple Dom elements at the same time.
+ *
+ * @example
+ * addInElements(myDom, { duration: 400 })
+ * await addInElements([myDom1, myDom2])
+ * await addInElements([myDom1, myDom2], { maxFadeIn: 0.75 })
+ * addInElements(myDom, { duration: 400, commitResult: false })
  *
  * @param {HTMLElementWithAnimations|Array<HTMLElementWithAnimations>} elements
  * @params {AnimationsOptions} options
@@ -336,7 +389,7 @@ export const addInElements = async(
   elements: HTMLElementWithAnimations|Array<HTMLElementWithAnimations>,
   options: AnimationsOptions = {},
 ): Promise<void> => {
-  const duration = options.duration || FADE_TRANSITION_DEFAULT_DURATION
+  const duration = options.duration || DEFAULT_FADE_TRANSITION_DURATION
   const maxFadeIn = options.maxFadeIn || 1
   const commitResult = options.commitResult || true
 
@@ -344,7 +397,14 @@ export const addInElements = async(
 }
 
 /**
+ * @function removeOutElements
  * Performs a removeOut animation on one or multiple Dom elements at the same time.
+ *
+ * @example
+ * removeOutElements(myDom, { duration: 400 })
+ * await removeOutElements([myDom1, myDom2])
+ * await removeOutElements([myDom1, myDom2], { maxFadeIn: 0.75 })
+ * removeOutElements(myDom, { duration: 400, commitResult: false })
  *
  * @param {HTMLElementWithAnimations|Array<HTMLElementWithAnimations>} elements
  * @params {AnimationsOptions} options
@@ -354,7 +414,7 @@ export const removeOutElements = async(
   elements: HTMLElementWithAnimations|Array<HTMLElementWithAnimations>,
   options: AnimationsOptions = {},
 ): Promise<void> => {
-  const duration = options.duration || FADE_TRANSITION_DEFAULT_DURATION
+  const duration = options.duration || DEFAULT_FADE_TRANSITION_DURATION
   const maxFadeIn = options.maxFadeIn || 1
   const commitResult = options.commitResult || true
 
