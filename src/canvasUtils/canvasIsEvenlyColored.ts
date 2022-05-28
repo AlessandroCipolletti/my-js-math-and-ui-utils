fea43ba3ed0380851194ffa408573e5f68b33a44
Alessandro Cipolletti <cipolletti.alessandro@gmail.com>
2022-05-29 01:34:36 +0200

    Update canvasIsEvenlyColored.ts

--- src/canvasUtils/canvasIsEvenlyColored.ts
+++ src/canvasUtils/canvasIsEvenlyColored.ts
@@ -11,25 +11,14 @@
  * @returns {boolean}
  */
 const canvasIsEvenlyColored = (destinationContext: MyCanvasRenderingContext2D): boolean => {
-  const data = destinationContext.getImageData(
+  const imageData = destinationContext.getImageData(
     0,
     0,
     destinationContext.canvas.width,
     destinationContext.canvas.height,
   ).data
-  const r = data[0], g = data[1], b = data[2], a = data[3]
-  let ok = true
-  let i = data.length
 
-  while (i--) {
-    if (!(data[i] === a && data[--i] === b && data[--i] === g && data[--i] === r)) {
-      ok = false
-      break
-    }
-  }
-
-  // ok = data.every((e, i, arr) => e === arr[i % 4])
-  return ok
+  return !imageData.some((color, i, data) => color !== data[i % 4])
 }
 
 
